1be20212abdb06aacca60ff2ed894e6520c2db5a
Jose Antonio González Santos <contact@devpullcode.dev>
2023-12-10 01:56:18 +0100

    feat: ✨ feat: Add dynamic icon toggle for theme switch button

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -1,9 +1,26 @@
+const iconTheme = document.querySelector('.m-theme-toggle__icon');
+const rootElem = document.documentElement;
+const dataTheme = rootElem.dataset.theme;
+
+if (dataTheme === 'dark') {
+  iconTheme.classList = 'fa-solid fa-sun fa-2xl m-theme-toggle__icon';
+} else {
+  iconTheme.classList = 'fa-solid fa-moon fa-2xl m-theme-toggle__icon';
+}
+
 const switchTheme = () => {
+  const iconTheme = document.querySelector('.m-theme-toggle__icon');
   const rootElem = document.documentElement;
   const dataTheme = rootElem.dataset.theme;
   let newTheme;
 
-  newTheme = dataTheme === 'light' ? 'dark' : 'light';
+  if (dataTheme === 'light') {
+    newTheme = 'dark';
+    iconTheme.classList = 'fa-solid fa-sun fa-2xl m-theme-toggle__icon';
+  } else {
+    newTheme = 'light';
+    iconTheme.classList = 'fa-solid fa-moon fa-2xl m-theme-toggle__icon';
+  }
   rootElem.dataset.theme = newTheme;
 
   // set local storage
