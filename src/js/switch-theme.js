e520e29a3ae1d0e8598fec3ffb727f8bbad8ac21
Jose Antonio González Santos <contact@devpullcode.dev>
2023-12-13 18:20:21 +0100

    enhancement: ✨ Switch icon modification to CSS rule in m-theme-toggle

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -1,13 +1,6 @@
 (() => {
-  const iconTheme = document.querySelector('.m-theme-toggle__icon');
   const rootElem = document.documentElement;
   const dataTheme = rootElem.dataset.theme;
-
-  if (dataTheme === 'dark') {
-    iconTheme.classList = 'fa-solid fa-sun fa-2xl m-theme-toggle__icon';
-  } else {
-    iconTheme.classList = 'fa-solid fa-moon fa-2xl m-theme-toggle__icon';
-  }
 
   // switchThemeIcons
   (() => {
@@ -27,17 +20,14 @@
     const computerLanguagesIcon2 = document.querySelector('.m-computer-languages__icon-body:last-child');
     const frontendMentorIconH = document.querySelector('.m-frontend-mentor__icon-header');
 
-    const iconTheme = document.querySelector('.m-theme-toggle__icon');
     const rootElem = document.documentElement;
     const dataTheme = rootElem.dataset.theme;
     let newTheme;
 
     if (dataTheme === 'light') {
       newTheme = 'dark';
-      iconTheme.classList = 'fa-solid fa-sun fa-2xl m-theme-toggle__icon';
     } else {
       newTheme = 'light';
-      iconTheme.classList = 'fa-solid fa-moon fa-2xl m-theme-toggle__icon';
     }
     rootElem.dataset.theme = newTheme;
 
